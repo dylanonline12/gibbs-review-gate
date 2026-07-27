877299f6d95bc6efa13d647b0ed19a09ef57d789
Dylan <dylanonline12@yahoo.com>
2026-07-27 17:25:54 -0400

    Update Google Review URL to exact Google Maps direct listing URL

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const CONFIG = {
   tagline: "Located in Acushnet, MA",
   foundedYear: "1950",
   
-  // Google Review direct URL
-  googleReviewUrl: "https://www.google.com/search?q=gibbs+roofing+and+remodeling&rlz=1C1RXQR_enUS1129US1129&oq=&gs_lcrp=EgZjaHJvbWUqCQgDECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyDwgCEC4YJxivARjHARjqAjIJCAMQIxgnGOoCMgwIBBAjGCcY6gIYrQYyDAgFECMYJxjqAhitBjIPCAYQABhCGLQCGOoCGNsFMg8IBxAAGEIYtAIY6gIY2wXSAQo3MzkxNjVqMGo3qAIIsAIB8QUpaeNUchXVpQ&sourceid=chrome&source=chrome.ob&ie=UTF-8#lrd=0x89e4e5d68ce37a53:0x9f9a291c8092f71d,3,,,,",
+  // Google Maps Direct Review Listing URL
+  googleReviewUrl: "https://www.google.com/maps/place/Gibbs+Roofing+and+Remodeling/@41.713573,-70.9006024,25961m/data=!3m1!1e3!4m8!3m7!1s0x89e4e5d68ce37a53:0x9f9a291c8092f71d!8m2!3d41.713573!4d-70.9006024!9m1!1b1!16s%2Fg%2F11rd9dk0nn",
 
   // Email destination for private feedback (1-4 stars)
   targetEmail: "Dylanonline12@yahoo.com",
